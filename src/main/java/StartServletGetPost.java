d6960a1bfaa19922e5e9d68aa9c3151711351be9
AnatolSalau <salovanatoly5@mail.ru>
2022-07-04 16:24:29 +0300

    first commit

--- src/main/java/StartServletGetPost.java
+++ src/main/java/StartServletGetPost.java
@@ -12,7 +12,7 @@ public class StartServletGetPost extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         PrintWriter printWriter = resp.getWriter();
-        printWriter.println("Hello");
+        printWriter.println("Hello from servlet");
     }
 
     @Override
